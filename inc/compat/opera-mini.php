7b030f0d887bcba0bb64405544fdfe53a50bd9b4
Maciej Gryniuk <maciejka45@gmail.com>
2015-09-30 18:05:28 +0200

    Prevent Undefined index: HTTP_USER_AGENT for opera-mini.php

--- inc/compat/opera-mini.php
+++ inc/compat/opera-mini.php
@@ -1,7 +1,7 @@
 <?php
 
 function bjll_compat_operamini() {
-	if ( false !== strpos( $_SERVER['HTTP_USER_AGENT'], 'Opera Mini' ) ) {
+	if ( isset( $_SERVER['HTTP_USER_AGENT'] ) && false !== strpos( $_SERVER['HTTP_USER_AGENT'], 'Opera Mini' ) ) {
 		add_filter( 'bjll/enabled', '__return_false' );
 	}
 }
