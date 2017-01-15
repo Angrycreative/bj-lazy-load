e66920ad47b7a64dc23f5cfd5b45011b6bbe8fe0
mustafauysal <m_uysl@hotmail.com>
2017-01-15 22:59:15 +0300

    amp compat added

---
+++ inc/compat/amp.php
@@ -0,0 +1,9 @@
+<?php
+
+function bjll_compat_amp() {
+	if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
+		add_filter( 'bjll/enabled', '__return_false' );
+	}
+}
+
+add_action( 'bjll/compat', 'bjll_compat_amp' );
