32792cf57d19fa045f398abf5ada1cc9b6fb8659
Rickard Berg <48469865+rickard-berg@users.noreply.github.com>
2019-07-01 11:45:11 +0200

    Changed the active AMP endpoints-check

--- src/inc/compat/amp.php
+++ src/inc/compat/amp.php
@@ -1,7 +1,7 @@
 <?php
 
 function bjll_compat_amp() {
-	if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
+	if ( ( function_exists( 'amp_activate' ) && defined( 'AMP__VERSION' ) ) && ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) ) {
 		add_filter( 'bjll/enabled', '__return_false' );
 	}
 }
